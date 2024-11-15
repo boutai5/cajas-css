4dd266e8b3ca5d412566d9678c743db54619c884
boutai5 <farisbel7@gmail.com>
2024-11-15 13:41:50 +0100

    js

---
+++ script.js
@@ -0,0 +1,43 @@
+alert("hola mundo");
+alert(1+9+41);
+alert("1+52+23");
+
+let saludo= "holaaa";
+
+let despedida;
+despedida = "adios";
+
+saludo ="hola!";
+
+ let nombreusuario = "boutaina";
+
+ alert( saludo+nombreusuario);
+
+
+//constantes
+const SALUDO_USUARIO = saludo+nombreUsuario; //adiosboutaina
+
+ saludo
+saludo = despedida;
+ alert(1+2);
+ alert(1-2);
+ alert(12/2);
+ alert(8*2);
+ alert(8%2);
+ alert(15%2);
+ let edad = 16;
+ edad = 17;
+ edad +1;
+ edad ++;
+ edad --;
+ edad += decada;
+
+ alert(edad);
+
+
+ 
+ //muestra lo que pongamos entre los parentesis en la consola
+ console.log(edad);
+
+
+
