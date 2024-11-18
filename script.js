a3aa0c9a44bdd86843e19418c38f4622abb21ce7
boutai5 <farisbel7@gmail.com>
2024-11-18 12:07:04 +0100

    javascript

--- script.js
+++ script.js
@@ -1,43 +1,64 @@
+
+//alert() muestra una ventana emergente
+/*
 alert("hola mundo");
 alert(1+9+41);
 alert("1+52+23");
+*/
+
+
+
 
 let saludo= "holaaa";
 
-let despedida;
-despedida = "adios";
 
+let despedida = "adios";
 saludo ="hola!";
 
+//igualar una variable al otra
  let nombreusuario = "boutaina";
 
  alert( saludo+nombreusuario);
 
-
 //constantes
-const SALUDO_USUARIO = saludo+nombreUsuario; //adiosboutaina
+const SALUDO_USUARIO = saludo + "boutaina";     //hola boutaina
 
  saludo
 saludo = despedida;
- alert(1+2);
- alert(1-2);
- alert(12/2);
- alert(8*2);
- alert(8%2);
+/*
+ alert(1+2);   //sumar
+ alert(1-2);   //restar
+ alert(12/2);  //dividir
+ alert(8*2);   //multiplicar
+ alert(8%2);   //resto de division
  alert(15%2);
+ */
+
+let decada = 10;
+
  let edad = 16;
  edad = 17;
- edad +1;
- edad ++;
- edad --;
+ edad +1;   //puedo sumar a la variable porque guarda el valor numerico
+ edad ++;  //suma uno
+ edad --;  //resta uno
  edad += decada;
+
+
 
  alert(edad);
 
-
- 
  //muestra lo que pongamos entre los parentesis en la consola
- console.log(edad);
+ console.log("hola");
 
+alert( saludo+" " +nombreusuario); //puedo concatenar el string usando+
+//no puede cambiar el valor
+const saludo_usuario = "hola de nuevo"
 
+console.log( 5 > 4);
+console.log( 5 < 8 );
 
+console.log( 5 >= 6 );
+console.log( 5 <= 7 );
+
+console.log( 5==5 );
+console.log( 5 !=7);
